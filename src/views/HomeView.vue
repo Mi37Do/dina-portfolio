bfec991cec10fa06fcabecacbfd2eecfd1195422
Mi37Do <36308654+Mi37Do@users.noreply.github.com>
2023-11-05 22:47:24 +0100

    heh

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,44 @@
-<script setup>
-import topNav from "../components/topNav.vue";
-</script>
 
 <template>
-  <main class="w-full h-fit flex justify-center relative">
+  <main class="w-full h-fit flex justify-center relative overflow-hidden">
 
-    <img src="@/assets/images/header.png" class="w-full absolute -top-[55%]" alt="">
+    <div class="w-[100%] md:w-full absolute h-[16%] lg:h-fit  top-0">
+      <img src="@/assets/images/header.png" class="w-full h-full object-cover" alt="">
+    </div>
+
     <div class="w-full max-w-screen-xl h-fit relative">
 
       <top-nav />
 
-      <div class="w-full h-fit flex flex-col  px-4 xl:px-8">
-        <section class="w-full h-screen flex flex-col gap-4 justify-center items-center">
-          <span class="text-[8rem] font-bold capitalize">dina fettache</span>
+      <div class="w-full h-fit flex flex-col  px-6 xl:px-8">
+        <section class="w-full h-screen flex flex-col gap-12 justify-center items-center overflow-hidden">
 
-          <!-- 
-          <span class="text-3xl font-semibold text-secondary">lorem ipsum</span>
-          <span class="w-fit text-center text-2xl text-slate-500">a full stack developer</span>
-          <div class="w-fit grid grid-cols-3 gap-4 mt-4">
-            <button class="btn btn-square"></button>
-            <button class="btn btn-square"></button>
-            <button class="btn btn-square"></button>
-          </div> -->
+          <img src="@/assets/images/nameStroke.png" class="w-[60%]">
+
+          <span class="md:w-[500px] text-justify">
+            <strong>full stack developer</strong>
+            specializing in building exceptional digital
+            experiences. With
+            perfect mastery of
+            HTML, CSS, JavaScript, and its framework React, Python and its framework Django. Currently, I’m focused on
+            building accessible, human-centered products.</span>
+          <div class="w-fit grid grid-cols-2 gap-4 mt-4">
+            <button class="btn btn-square btn-ghost">
+              <linkedin-icon class="w-6 h-6 fill-white" />
+            </button>
+            <button class="btn btn-square btn-ghost">
+              <github-icon class="w-6 h-6 fill-white" />
+            </button>
+          </div>
         </section>
-        <section class="w-full h-screen bg-violet-200">
+        <section class="w-full h-screen flex justify-center">
+          <div class="w-fit flex flex-col gap-4">
+            <span class="text-3xl font-bold uppercase">information technology</span>
+            <div class="w-full h-fit flex items-end gap-6">
+              <span class="flex-1 h-px bg-white mb-0.5"></span>
+              <span>skills</span>
+            </div>
+          </div>
 
         </section>
         <section class="w-full h-screen bg-violet-100">
@@ -35,3 +50,8 @@ import topNav from "../components/topNav.vue";
     </div>
   </main>
 </template>
+<script setup>
+import githubIcon from "../assets/icons/githubIcon.vue";
+import linkedinIcon from "../assets/icons/linkedinIcon.vue";
+import topNav from "../components/topNav.vue";
+</script>
