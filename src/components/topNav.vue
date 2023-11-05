3d0903b1239edf014d969418daebdf3221ec2507
Mi37Do <36308654+Mi37Do@users.noreply.github.com>
2023-11-05 02:12:20 +0100

    hh

---
+++ src/components/topNav.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="w-full h-16 bg-red-600 sticky inset-0">
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<script setup>
+import topNav from "../components/topNav.vue";
+</script>
+
+<template>
+  <main class="w-full h-fit bg-red-200 flex justify-center">
+
+
+    <div class="w-full max-w-screen-xl h-fit bg-white relative">
+
+      <top-nav />
+
+      <div class="w-full h-fit flex flex-col  px-4 xl:px-8">
+        <section class="w-full h-screen bg-violet-400">
+
+        </section>
+        <section class="w-full h-screen bg-violet-200">
+
+        </section>
+        <section class="w-full h-screen bg-violet-100">
+
+        </section>
+      </div>
+
+
+    </div>
+  </main>
+</template>
